ffc9dc49e30382f7ccfa34684ca9dbc7225d18a8
Dikeh <onyekachi.okomba@gmail.com>
2024-07-02 18:45:30 -0700

    Intalling a package using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # install flask version 2.3
 # Ensure python3-pip is installed
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
